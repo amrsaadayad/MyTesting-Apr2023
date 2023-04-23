7731cb0003cf128c5a1e6eb3d49152776b15db99
amrsaadayad <amr_saad_30@hotmail.com>
2023-04-23 16:40:08 +1000

    Add files via upload

---
+++ myfactorial.py
@@ -0,0 +1,15 @@
+#testing python, writing a factorial code
+#code will get an input number no more than 15 and output the factorial
+user_input = input("Enter an integer: ")
+try:
+    x = int(user_input)
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+if(x>15):
+    print("too big for me")
+    exit()
+Fact=1
+for i in range (1,x+1):
+    Fact=Fact*i
+    i+=1
+print("the factorial is", Fact)
